5f34afc45a0b05e7ed133cd8fda39847d77402bf
Harry Guan <guanhuai2014@gmail.com>
2024-09-20 13:47:23 -0500

    Implemented add_points function

--- app.py
+++ app.py
@@ -7,13 +7,51 @@ app = Flask(__name__)
 
 lock = threading.Lock()
 
-balanced = {}
+balances = {}
 transactions = deque()
 
 
 @app.route('/add', methods=['POST'])
 def add_points():
-    pass
+    data = request.get_json()
+    fields = {'payer', 'points', 'timestamp'}
+    if not data or not fields.issubset(data.keys()):
+        abort(400, 'Request must contain payer, points, and timestamp fields')
+    
+    payer = data['payer']
+    points = data['points']
+    timestamp_str = data['timestamp']
+    
+    try:
+        timestamp = datetime.strptime(timestamp_str, '%Y-%m-%dT%H:%M:%SZ') # convert to datetime object
+    except ValueError:
+        abort(400, 'Timestamp must be in format YYYY-MM-DDTHH:MM:SSZ')
+        
+    with lock:
+        # update balances
+        balances[payer] = balances.get(payer, 0) + points
+
+        # check for negative balance
+        if balances[payer] < 0:
+            abort(400, f'Payer {payer} cannot have negative balance.')
+
+        # insert transaction in sorted order
+        transaction = {
+            'payer': payer,
+            'points': points,
+            'timestamp': timestamp
+        }
+        # insert transaction, while maintaining sorted order
+        inserted = False
+        for idx, existing_txn in enumerate(transactions):
+            if transaction['timestamp'] < existing_txn['timestamp']:
+                transactions.insert(idx, transaction)
+                inserted = True
+                break
+        if not inserted:
+            transactions.append(transaction)
+
+    return '', 200
 
 @app.route('/spend', methods=['POST'])
 def spend_points():
